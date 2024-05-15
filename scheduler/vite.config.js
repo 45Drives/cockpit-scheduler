b5e0b12f0f9f916039b8ff1a7598a211b8c1aeea
joshuaboud <joshuaboud@hotmail.com>
2024-05-15 13:01:51 -0300

    dedupe vue library

--- scheduler/vite.config.js
+++ scheduler/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "./",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    dedupe: ['vue'],
+  },
   build: {
     minify: false,
   }
