b34caa48977eba13ff137c32f42262e30d0f0df6
JordanKeo45 <jkeough@45drives.com>
2024-05-17 15:25:51 -0300

    updates bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+# bootstrap.sh
 
 set -e
 set -o pipefail
+set -x
+
+command -v sponge >/dev/null || { echo "Missing 'sponge'. Please install moreutils." >&2 ; exit 1 ; }
 
 git submodule update --init
 
 jq 'del(.packageManager)' ./package.json | sponge ./package.json
 
-# rm .yarnrc.yml .yarn -rf
+rm .yarnrc.yml .yarn -rf
 
 yarn set version stable
 
@@ -15,6 +19,6 @@ yarn config set nodeLinker node-modules
 
 yarn install
 
-yarn workspace @45drives/houston-common install
+yarn workspaces foreach -A install
 
 yarn build:common
