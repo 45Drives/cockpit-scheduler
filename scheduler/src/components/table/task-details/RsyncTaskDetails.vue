27de44f742fab8196af6a09daab13e4459df9c99
JordanKeo45 <jkeough@45drives.com>
2024-06-07 09:20:34 -0300

    adds more info to RsyncTaskDetails

--- scheduler/src/components/table/task-details/RsyncTaskDetails.vue
+++ scheduler/src/components/table/task-details/RsyncTaskDetails.vue
@@ -5,16 +5,19 @@
             <p class="my-2 truncate" :title="`Task Type: ${taskInstance.template.name}`">
                 Task Type: <b>Rsync Task</b>
             </p>
-            <p class="my-2 truncate" :title="`Send Type: ${findValue(taskInstance.parameters, 'target_info', 'host') !== '' ? 'Remote' : 'Local'}`">
-                Transfer Type:
-                <b v-if="findValue(taskInstance.parameters, 'target_info', 'host') !== ''">Remote</b>
-                <b v-if="findValue(taskInstance.parameters, 'target_info', 'host') === ''">Local</b>
+            <p class="my-2 truncate" :title="`Direction: ${upperCaseWord(findValue(taskInstance.parameters, 'direction', 'direction'))}`">
+                Direction: <b>{{ upperCaseWord(findValue(taskInstance.parameters, 'direction', 'direction')) }}</b>
             </p>
             <p class="my-2 truncate" :title="`Local Path: ${findValue(taskInstance.parameters, 'local_path', 'local_path')}`">
                 Local Path: <b>{{ findValue(taskInstance.parameters, 'local_path', 'local_path') }}</b>
             </p>
             <p class="my-2 truncate" :title="`Target Path: ${findValue(taskInstance.parameters, 'target_info', 'path')}`">
                 Target Path: <b>{{ findValue(taskInstance.parameters, 'target_info', 'path') }}</b>
+            </p>
+            <p class="my-2 truncate" :title="`Send Type: ${findValue(taskInstance.parameters, 'target_info', 'host') !== '' ? 'Remote' : 'Local'}`">
+                Transfer Type:
+                <b v-if="findValue(taskInstance.parameters, 'target_info', 'host') !== ''">Remote</b>
+                <b v-if="findValue(taskInstance.parameters, 'target_info', 'host') === ''">Local</b>
             </p>
             <p class="my-2"
                 v-if="findValue(taskInstance.parameters, 'target_info', 'host') !== ''">
@@ -27,9 +30,7 @@
             </p>
         </div>
         <div class="col-span-1">
-            <p class="my-2 truncate" :title="`Direction: ${upperCaseWord(findValue(taskInstance.parameters, 'direction', 'direction'))}`">
-                Direction: <b>{{ upperCaseWord(findValue(taskInstance.parameters, 'direction', 'direction')) }}</b>
-            </p>
+           
             <p class="my-2 truncate" :title="`Archive Mode: ${boolToYesNo(findValue(taskInstance.parameters, 'rsyncOptions', 'archive_flag'))}`">
                 Archive Mode: <b>{{ boolToYesNo(findValue(taskInstance.parameters, 'rsyncOptions', 'archive_flag')) }}</b>
             </p>
@@ -39,12 +40,12 @@
             <p class="my-2 truncate" :title="`Compressed: ${boolToYesNo(findValue(taskInstance.parameters, 'rsyncOptions', 'compressed_flag'))}`">
                 Compressed: <b>{{ boolToYesNo(findValue(taskInstance.parameters, 'rsyncOptions', 'compressed_flag')) }}</b>
             </p>
-            <!-- <p class="my-2 truncate" :title="`: ${findValue(taskInstance.parameters, '', '')}`">
-                : <b>{{ findValue(taskInstance.parameters, '', '') }}</b>
+            <p class="my-2 truncate" :title="`Bandwidth Limit: ${((findValue(taskInstance.parameters, 'rsyncOptions', 'bandwidth_limit_kbps') !== 0 ? `${findValue(taskInstance.parameters, 'rsyncOptions', 'bandwidth_limit_kbps')} kb/s` : 'No'))}`">
+                Bandwidth Limit: <b>{{ ((findValue(taskInstance.parameters, 'rsyncOptions', 'bandwidth_limit_kbps') !== 0 ? `${findValue(taskInstance.parameters, 'rsyncOptions', 'bandwidth_limit_kbps')} kb/s` : 'No')) }}</b>
             </p>
-            <p class="my-2 truncate" :title="`: ${findValue(taskInstance.parameters, '', '')}`">
-                : <b>{{ findValue(taskInstance.parameters, '', '') }}</b>
-            </p> -->
+            <p class="my-2 truncate" :title="`Parallel Threads: ${(findValue(taskInstance.parameters, 'rsyncOptions', 'parallel_threads') !== 0 ? findValue(taskInstance.parameters, 'rsyncOptions', 'parallel_threads') : 'No')}`">
+                Parallel Threads: <b>{{ (findValue(taskInstance.parameters, 'rsyncOptions', 'parallel_threads') !== 0 ? findValue(taskInstance.parameters, 'rsyncOptions', 'parallel_threads') : 'No') }}</b>
+            </p>
         </div>
         <div class="col-span-2 row-span-2">
             <p class="my-2 font-bold">Current Schedules:</p>
