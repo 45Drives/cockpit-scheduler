68c47e1370a1ea982f0b51070ef105fd26b3a767
hansrachit123 <rhans@protocase.com>
2025-01-16 16:29:14 -0400

    Added code for displaying recent logs for sucessful tasks

--- scheduler/src/models/TaskLog.ts
+++ scheduler/src/models/TaskLog.ts
@@ -14,7 +14,6 @@ export class TaskExecutionLog {
         const taskName = taskInstance.name;
 
         const fullTaskName = `${houstonSchedulerPrefix}${templateName}_${taskName}`;
-
         try {
             if (!untilTime) {
                 console.log("No until time provided");
@@ -53,8 +52,8 @@ export class TaskExecutionLog {
 
             const properties = Object.fromEntries(execResult.stdout.split('\n').filter(line => line.includes('=')).map(line => line.split('=')));
             const exitCode = properties['ExecMainStatus'] || '0';
-            const startTime = properties['ExecMainStartTimestamp'] || '';
-            const finishTime = properties['ExecMainExitTimestamp'] || '';
+            let startTime = properties['ExecMainStartTimestamp'] || '';
+            let finishTime = properties['ExecMainExitTimestamp'] || '';
 
             let output = "";
             if (startTime) {
@@ -63,7 +62,48 @@ export class TaskExecutionLog {
                 const logResult = await logState.promise();
                 output = logResult.stdout;
             } else {
-                output = "No executions found.";
+                // If no `ExecMainStartTimestamp`, fetch the latest "Starting" entry from journalctl.
+                const journalCommand = [
+                    'bash',
+                    '-c',
+                    `journalctl -u ${fullTaskName} --no-pager --output=short-full | grep "Starting" | tail -n 1 | awk '{print $1, $2, $3, $4}'`
+                ];
+                const journalState = useSpawn(journalCommand, { superuser: 'try' });
+                const journalResult = await journalState.promise();
+                const lastRun = journalResult.stdout.trim();
+                startTime = lastRun;
+                const finishingCommand = [
+                    'bash',
+                    '-c',
+                    `journalctl -u ${fullTaskName} --no-pager --output=short-full | grep "Succeeded" | tail -n 1 | awk '{print $1, $2, $3, $4}'`
+                ];
+                const finishingState = useSpawn(finishingCommand, { superuser: 'try' });
+                const finishingResult = await finishingState.promise();
+                finishTime = finishingResult.stdout.trim();
+                let logResult;
+                if(lastRun){
+                    const formattedLastRun = lastRun.replace(/^\w{3}\s|\s[A-Z]{3}$/g, '').trim();
+                     const logCommand = [
+                         'journalctl',
+                         '-u',
+                         fullTaskName,
+                         '--no-pager',
+                         '--all',
+                         '--since',
+                         formattedLastRun
+                     ];
+
+                const logState = useSpawn(logCommand, { superuser: 'try' });
+                logResult = await logState.promise();
+                }
+      
+
+                if (logResult) {
+                    output = logResult.stdout;
+                    
+                } else {
+                    output = "No executions found.";
+                }
 
                 // Fallback to get the most recent log entries if no start time is available
                 /*   const logCommand = ['journalctl', '-r', '-u', fullTaskName, '--no-pager', '--all', '-n', '1']; // Limit to the most recent log entry
@@ -71,7 +111,6 @@ export class TaskExecutionLog {
                   const logResult = await logState.promise();
                   output = logResult.stdout || "Task hasn't run since boot or is disabled."; */
             }
-
             const latestEntry = new TaskExecutionResult(exitCode, output, startTime, finishTime);
             // console.log('latest entry:', latestEntry);
             return latestEntry;
